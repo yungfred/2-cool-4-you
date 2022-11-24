2b7ee4650991d85e383e76e7f3794d744c01f675
Benjamin Schaden <benni.schaden@gmail.com>
2022-11-24 12:54:45 +0100

    fixed userId search

--- web/cs.js
+++ web/cs.js
@@ -1,39 +1,11 @@
 // content script
 console.log("cs loaded");
 
-/*
- * searches DOM for a specific script tag
- * looks up userID in script content
- * */
-function getUserID(){
-  var codeSnippet;
-  //search for the script tag
-  for(i = 0; i<document.scripts.length; i++){
-    if(document.scripts[i].innerHTML.startsWith('window._sharedData')){
-      codeSnippet = document.scripts[i].innerHTML;
-    }
-  }
-  //console.log(codeSnippet);
-  
-  if(!codeSnippet){
-    console.warn("code snippet could not be found");
-    return 0;
-  }
-
-  //drop first chars until '{'
-  var c = 0;
-  while(codeSnippet.charAt(0) !== '{' && c < 30){
-    codeSnippet = codeSnippet.substr(1);
-    c++;
-  }
-  // drop last semicolon
-  codeSnippet = codeSnippet.substr(0,codeSnippet.length - 1);
-
-  var codeAsJson = JSON.parse(codeSnippet);
-  userID = codeAsJson.config.viewerId;
-
-  console.log("instagram userid: " + userID);
-  return userID;
+/* searches for userID in DOM */
+function getUserID() {
+  const fullHtml = document.documentElement.outerHTML;
+  const matches = fullHtml.match("(?<=viewerId\":\")\\d+(?=\")");
+  return matches[0];
 }
 
 // send userID to background script on cs loading
